074113a4c086f1da7db26e1a72a94ff82c607dfa
Aaditya Gupta <aadityagupta400@gmail.com>
2024-12-28 15:55:44 +0530

    conf: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'http://localhost:5174', 
-    credentials: true,              
-  });
+  app.enableCors();
 
   await app.listen(process.env.PORT ?? 3000);
 }
